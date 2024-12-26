b17ee77dee2b8bacf17a2578e18a4d58b04db527
ymurillo89 <murilloruiz91@gmail.com>
2024-12-26 07:50:05 -0500

    Uso de los custom hooks

--- src/page/GetCat/Cat.tsx
+++ src/page/GetCat/Cat.tsx
@@ -1,32 +1,17 @@
-import { useEffect, useState } from "react";
-import { getDataEcho } from "../../service/catService";
-import { useCatImage } from "../../customHook/catImage";
-
-//Custom Hook
+import { useCatImage, useEcho } from "../../customHook/catImage";
 
 
 function Cat() {
 
-  const [catFact, setCatFact] = useState<string>("");
+  const {catFact,dataFact} = useEcho();
   
   const imageCat = useCatImage({catFact});
-  
-  const getEcho = async() => {
-    const data = await getDataEcho();    
-    setCatFact(data);    
-  }
-
-  useEffect(() => {   
-    getEcho();
-  }, []);
-  
 
   return (
     <div>
       <h1>{catFact}</h1>
       <img src={imageCat} alt="Gatitos" height={300} width={300}/>
-
-      <button className="bg-red-500 rounded p-4 border text-white" onClick={getEcho} >Reload</button>
+      <button className="bg-red-500 rounded px-4 py-2 border text-white" onClick={dataFact} >Reload</button>
     </div>
   );
 
